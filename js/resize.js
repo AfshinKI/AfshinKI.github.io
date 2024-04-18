934521dc347d84882b2998d8efe9982b82ff25e3
SleepyCat-00 <mkuroda987@gmail.com>
2024-04-18 10:30:26 -0600

    Header width set to the container width. * Move js functions to functions.js file * Set background color to black * Added .gitignore

--- js/resize.js
+++
@@ -1,9 +0,0 @@
-function resizeSection(elementIds) {
-    elementIds.forEach(function(elementId) {
-    var element = document.getElementById(elementId);
-    var windowHeight = window.innerHeight;
-    element.style.height = windowHeight + 'px';
-    });
-}
-
-

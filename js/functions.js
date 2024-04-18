95a98b8a191538880b1c539d4a2ab5701f124dda
SleepyCat-00 <mkuroda987@gmail.com>
2024-04-18 10:30:26 -0600

    Header width set to the container width. * Move js functions to functions.js file * Set background color to black * Added .gitignore

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,18 @@ function setBackgroundImage(imageUrl, id) {
         element.style.backgroundPosition = "center";
     });
 }
+
+function resizeSection(elementIds) {
+    elementIds.forEach(function(elementId) {
+    var element = document.getElementById(elementId);
+    var windowHeight = window.innerHeight;
+    element.style.height = windowHeight + 'px';
+    });
+}
+
+function adjustWidth(elementId, referenceElementId){
+    var element = document.getElementById(elementId);
+    var referenceElement = document.getElementById(referenceElementId);
+    var referenceWidth = referenceElement.offsetWidth;
+    element.style.width = referenceWidth + 'px';
+}
